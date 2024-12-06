63e54c48aa8a346f4385ea167c3ce780c4edf3d0
Andres Kessler <andresgabriel.kessler@gmail.com>
2024-12-06 14:59:23 -0300

    primer commit

---
+++ landing-page/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Header from "./components/Header";
+import Hero from "./components/Hero"
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className="font-sans">
+        <Header />
+        <main>
+          <Hero />
+          <div>
+            
+          </div>
+        </main>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ landing-page/src/components/Hero.jsx
@@ -0,0 +1,22 @@
+export default function Hero() {
+  return (
+    <section id="inicio" className="relative h-screen overflow-hidden">
+      <div className="absolute inset-0 w-full h-full transition-opacity duration-1000 opacity-100">
+        <img
+          className="object-cover w-full h-full"
+          src="src/assets/hero.png"
+        ></img>
+      </div>
+      <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center">
+        <div className="text-center text-white sapce-y-8">
+          <div>
+            <h1 className="text-5xl font-bold mb-4">
+              Bienvenido a Digiano Asesores
+            </h1>
+            <p className="text-xl">Asegurando tranquilidad</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
